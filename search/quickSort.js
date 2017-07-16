900b949192f5262f9e4dec3fd196f89cdd71930a
Mikhail Novikov <eshifrin@gmail.com>
2017-07-16 16:32:54 -0700

    quicksort formatting

--- search/quickSort.js
+++ search/quickSort.js
@@ -1,63 +1,41 @@
-//--------------------------------SETUP---------------------------------------//
+// --------------------------------SETUP---------------------------------------//
+const testSuite = require('./main.js');
 
-// var testSuite =  require('./main.js')
+/*
+  step 1: if examining an array of less than length 2 return the array
+  step 2: initialize start as 0, end (aka pivot element) as arr.length - 1 if it doesnt exist
+  step 3: rearrange the elements from start to end so that the pivot element is sorted
+    (smaller elements to its left, larger to its right)
+  step 4: repeat steps 1 through 3 on the subarrays to the left and right of the pivot
+*/
 
-// /* 
-//   step 1: if examining an array of less than length 2 return the array
-//   step 2: initialize start as 0, end (aka pivot element) as arr.length - 1 if it doesnt exist
-//   step 3: rearrange the elements from start to end so that the pivot element is sorted (smaller elements to its left, larger to its right)
-//   step 4: repeat steps 1 through 3 on the subarrays to the left and right of the pivot
-// */
+// --------------------------------FUNCTIONS---------------------------------------//
 
-// //--------------------------------FUNCTIONS---------------------------------------//
+const partition = (arr, start, end) => {
+  const pivot = arr[end];
+  let lastIndexSmallerThanPivot = start - 1;
 
-// const partition = (arr, start, end) => {
+  for (let j = start; j < end; j += 1) {
+    if (arr[j] <= pivot) {
+      lastIndexSmallerThanPivot += 1;
+      arr.swap(lastIndexSmallerThanPivot, j);
+    }
+  }
+  arr.swap(end, lastIndexSmallerThanPivot + 1);
 
-//   const pivot = arr[end];
-//   let lastIndexSmallerThanPivot = start - 1;
+  return lastIndexSmallerThanPivot + 1;
+};
 
-//   for (let j = start; j < end; j++) {
-//     if (arr[j] <= pivot) {
-//       lastIndexSmallerThanPivot++;
-//       arr.swap(lastIndexSmallerThanPivot, j);
-//     }
-//   }
-//   arr.swap(end, lastIndexSmallerThanPivot + 1);
-
-//   return lastIndexSmallerThanPivot + 1;
-// }
-
-// const quickSort = (arr, start = 0, end = arr.length - 1) => {
-//   if (end - start <= 0) {
-//     return arr;
-//   }
-
-//   let pivot = partition(arr, start, end);
-//   quickSort(arr, start, pivot - 1);
-//   quickSort(arr, pivot + 1, end);
-
-//   return arr;
-// }
-
-
-
-//--------------------------------TEST------------------------------------------//
-
-// testSuite(quickSort)
-
-
-const avg = (n, start) => {
-  let arr = [];
-
-  for (let i = 0; i < n; i++) {
-    arr.push(Math.max(i - 1, n - i - 1))
+const quickSort = (arr, start = 0, end = arr.length - 1) => {
+  if (end - start <= 0) {
+    return arr;
   }
 
-  arr[Math.floor(n / 2)] = 0
+  const pivot = partition(arr, start, end);
+  quickSort(arr, start, pivot - 1);
+  quickSort(arr, pivot + 1, end);
 
-  console.log(arr)
-
-}
-
-avg(4)
-
+  return arr;
+};
+// --------------------------------TEST------------------------------------------//
+testSuite(quickSort);
