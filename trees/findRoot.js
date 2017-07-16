2cd6a92faa038f4b3089e8348ca42bf2cb84b654
Mikhail Novikov <eshifrin@gmail.com>
2017-07-16 16:39:01 -0700

    find root formatting

--- trees/findRoot.js
+++ trees/findRoot.js
@@ -1,45 +1,32 @@
-//--------------------------------SETUP---------------------------------------//
-
+// --------------------------------SETUP---------------------------------------//
 /*
 
 given an array of nodes find the root
 
 */
 
-class node {
+class Node {
   constructor(val) {
     this.val = val;
     this.left = null;
     this.right = null;
   }
-};
+}
 
-const assert = (str, expected, processed) => {
-  if (JSON.stringify(expected) === JSON.stringify(processed)) {
-    console.log(str, 'passed');
-  } else {
-    console.log(str, `failed - expected: ${expected} | got ${processed}`);
-  }
-};
+const assert = require('../helpers.js');
 
-//--------------------------------FUNCTIONS---------------------------------------//
-
+// --------------------------------FUNCTIONS---------------------------------------//
 const findRoot = (arr) => {
-  let m = new Set(arr);
+  const m = new Set(arr);
 
-  m.forEach(node => {
+  m.forEach((node) => {
     m.delete(node.left);
     m.delete(node.right);
   });
 
-
   return [...m][0];
-}
-
-
-
-//--------------------------------TESTS---------------------------------------//
-
+};
+// --------------------------------TESTS---------------------------------------//
   //     4
   //    /  \
   //   2    5
@@ -47,13 +34,13 @@ const findRoot = (arr) => {
   //  1  3 4 4
   // */
 
-let four = new node(4);
-let fourDupe = new node(4);
-let fourDupeTwo = new node(4);
-let two = new node(2);
-let five = new node(5);
-let one = new node(1);
-let three = new node(3);
+const four = new Node(4);
+const fourDupe = new Node(4);
+const fourDupeTwo = new Node(4);
+const two = new Node(2);
+const five = new Node(5);
+const one = new Node(1);
+const three = new Node(3);
 
 four.left = two;
 four.right = five;
@@ -63,10 +50,5 @@ four.right.left = fourDupeTwo;
 four.left.left = one;
 four.left.right = three;
 
-let nodeArr = [two, five, fourDupe, fourDupeTwo, four, one, three];
-
+const nodeArr = [two, five, fourDupe, fourDupeTwo, four, one, three];
 assert('one test findRoot_set', four, findRoot(nodeArr));
-assert('one test findRoot_obj', four, findRoot2(nodeArr));
-
-
-
