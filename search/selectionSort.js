8c31025a62b65d9780653c91dbe72c2ae5651007
Mikhail Novikov <eshifrin@gmail.com>
2017-07-16 16:35:00 -0700

    selectionsort formatting

--- search/selectionSort.js
+++ search/selectionSort.js
@@ -1,24 +1,20 @@
-//--------------------------------SETUP---------------------------------------//
-
-var testSuite =  require('./main.js')
-
-/* 
+// --------------------------------SETUP---------------------------------------//
+const testSuite = require('./main.js');
+/*
   selection sort
-  
+
   loop through i = 0 to array.length - 1
   find the smallest element from i to end of array, swap it with element @ i
   always n^2
 
 */
-
-//--------------------------------FUNCTIONS---------------------------------------//
-
+// --------------------------------FUNCTIONS---------------------------------------//
 const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i += 1) {
     let minIndex = i;
     let minElement = arr[i];
 
-    for (let j = i + 1; j < arr.length; j++) {
+    for (let j = i + 1; j < arr.length; j += 1) {
       if (arr[j] < minElement) {
         minIndex = j;
         minElement = arr[j];
@@ -29,9 +25,6 @@ const selectionSort = (arr) => {
   }
 
   return arr;
-}
-
-//--------------------------------TEST------------------------------------------//
-
-testSuite(selectionSort)
-
+};
+// --------------------------------TEST------------------------------------------//
+testSuite(selectionSort);
